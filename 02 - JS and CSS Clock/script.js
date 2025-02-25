e32314c2fc7e32af2640ecfd1f089b94cc490481
Aaditya Raikar <aadityaraikar73@gmail.com>
2025-02-25 14:01:29 +0530

    chore: add js and css clock project

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const $hourHand = document.querySelector('.hour-hand');
+const $minHand = document.querySelector('.min-hand');
+const $secondHand = document.querySelector('.second-hand');
+
+const setDate = () => {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  $secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  $minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour % 12) * 30 + (mins / 60) * 30 + 90;
+  $hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+};
+
+setInterval(setDate, 1000);
